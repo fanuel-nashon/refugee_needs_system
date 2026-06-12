267c87670b7bbb27867602a5ebc4acb8ebddd1a3
fanuel-nashon <fanuelnashon517@gmail.com>
2026-06-12 12:21:17 +0300

    feat: seed the database with country
    These are the countries that the system will use as refugee origin and refugee destination. They comprise of African leading countries in both combinations

--- database/seeders/CountrySeeder.php
+++ database/seeders/CountrySeeder.php
@@ -13,18 +13,30 @@ class CountrySeeder extends Seeder
      */
     public function run(): void
     {   
-        //seeding some countries to the database (picked them from the list of top 10 African countries with the highest number of refugees)
+        //leading african countries in refugee emigration in Africa that will be used as origin countries for refugees in the system
         $countries = [
             ['name' => 'Ethiopia'],
-            ['name' => 'Uganda'],
             ['name' => 'Sudan'],
-            ['name' => 'Kenya'],
-            ['name' => 'DR Congo'],
-            ['name' => 'Chad'],
-            ['name' => 'Libya'],
             ['name' => 'Somalia'],
+            ['name' => 'Democratic Republic of Congo'],
+            ['name' => 'Central African Republic'],
+            ['name' => 'South Sudan'],
+            ['name' => 'Burundi'],
             ['name' => 'Niger'],
-            ['name' => 'Cameroon'],
+            ['name' => 'Mali'],
+            ['name' => 'Chad'],
+
+            // adding countries that are leading in refugee resettlement in Africa that will be used as destination countries for refugees in the system
+            ['name' => 'South Africa'],
+            ['name' => 'Tanzania'],
+            ['name' => 'Uganda'],
+            ['name' => 'Kenya'],
+            ['name' => 'Rwanda'],
+            ['name' => 'Ghana'],
+            ['name' => 'Senegal'],
+            ['name' => 'Ivory Coast'],
+            ['name' => 'Zambia'],
+            ['name' => 'Zimbabwe'],
         ];
 
         //creating them into the database using Country model
