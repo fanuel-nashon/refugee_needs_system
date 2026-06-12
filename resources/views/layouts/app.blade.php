0fd4694c4390ecdd109da6c38e3f30b7572e16a6
fanuel-nashon <fanuelnashon517@gmail.com>
2026-06-12 13:05:14 +0300

    feat: import tailwind in layouts

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Refugee Needs System</title>
+    {{-- importing the stylesheet that contains Tailwind CSS --}}
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    
 </head>
 <body>
     
