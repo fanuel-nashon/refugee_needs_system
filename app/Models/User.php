f11584232648c939d3533e86184ca991782abc15
fanuel-nashon <fanuelnashon517@gmail.com>
2026-06-12 12:24:52 +0300

    feat: add the HasRoles Tratit to enable RBAC

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,6 +12,7 @@ use Spatie\Permission\Traits\HasRoles;
 class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
+    // added the HasRoles trait to the User model to enable role-based access control in the system
     use HasFactory, Notifiable, HasRoles;
 
     /**
